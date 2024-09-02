15e3756c2b5aa2533e61b0ab0e9e20ac4c4aca56
Kompal Poorkar <poorkarkompal22@gmail.com>
2024-09-02 23:35:30 +0530

    Routing setup and components created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import { useState } from 'react'
 import './App.css'
-
+import Portfolio from './components/Herosection.jsx'
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+import Herosection from './components/Herosection.jsx';
+import About from './components/About.jsx';
+import Resume from './components/Resume.jsx';
+import Contact from './components/Contact.jsx';
+import Projects from './components/Projects.jsx';
 function App() {
 
-
+  <Routes>
+    <Route path='/' element={<Herosection />} />
+    <Route path='/about' element={<About />} />
+    <Route path='/resume' element={<Resume />} />
+    <Route path='/contact' element={<Contact />} />
+    <Route path='/projects' element={<Projects />} />
+  </Routes>
+  
   return (
     <>
-    <h1 className='bg-violet-300'>Portfolio</h1>
+      <BrowserRouter>
+        <Portfolio />
+      </BrowserRouter>
     </>
   )
 }
