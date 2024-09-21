a38dc167f84d217f60c111cad1553396c4cb9bbc
Kompal Poorkar <poorkarkompal22@gmail.com>
2024-09-21 15:47:42 +0530

    left section complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,15 @@
-import { useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Index from './components/Index.jsx';
 import './App.css'
-import Portfolio from './components/Herosection.jsx'
-import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
-import Herosection from './components/Herosection.jsx';
-import About from './components/About.jsx';
-import Resume from './components/Resume.jsx';
-import Contact from './components/Contact.jsx';
-import Projects from './components/Projects.jsx';
-function App() {
 
-  <Routes>
-    <Route path='/' element={<Herosection />} />
-    <Route path='/about' element={<About />} />
-    <Route path='/resume' element={<Resume />} />
-    <Route path='/contact' element={<Contact />} />
-    <Route path='/projects' element={<Projects />} />
-  </Routes>
-  
+function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Portfolio />
-      </BrowserRouter>
-    </>
-  )
+    <div className='box-border '>
+    <BrowserRouter>
+      <Index />
+    </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
